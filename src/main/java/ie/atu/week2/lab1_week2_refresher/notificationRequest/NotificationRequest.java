608beff8d62326ec736bbb4fe70342d2dda7ec11
Mark Veerasingam <markveer70@gmail.com>
2024-10-05 17:38:27 +0100

    connected to microservice 2 notification

--- src/main/java/ie/atu/week2/lab1_week2_refresher/notificationRequest/NotificationRequest.java
+++ src/main/java/ie/atu/week2/lab1_week2_refresher/notificationRequest/NotificationRequest.java
@@ -1,5 +1,6 @@
 package ie.atu.week2.lab1_week2_refresher.notificationRequest;
 
+import ie.atu.week2.lab1_week2_refresher.Product;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class NotificationRequest {
     private String action; // i.e. created/deleted/updated
-    private String entityName; // in this case it is product
     private long entityId; // in this case it is the product ID
+    private String entityName; // Name of the product
+    private Double entityPrice; // Price of the product
 }
